f819b7e804cb81d4a4009bbed8d20caafc3453e2
Fredrik Nilsson <fredrik.karlsbyn@gmail.com>
2022-10-29 11:22:49 +0200

    Initial Commit

---
+++ Mo4Le15Ch/Mo4Le15ChApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mo4Le15ChApp.swift
+//  Mo4Le15Ch
+//
+//  Created by Fredrik Nilsson on 2022-10-29.
+//
+
+import SwiftUI
+
+@main
+struct Mo4Le15ChApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
